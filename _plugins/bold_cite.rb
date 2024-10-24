2459898883c8977ff6cc4bdef134f69513a082fe
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-10-24 13:35:03 -0400

    Reformat

--- _plugins/bold_cite.rb
+++ _plugins/bold_cite.rb
@@ -1,25 +1,24 @@
 module Jekyll
-    class RenderBoldCiteTagBlock < Liquid::Block
-      def render(context)
-        raw_text = super
-        context.registers[:site].collections["people"].docs.each do |person|
-            person_names = [person.data["name"]]
-            person_names += person.data["pub_names"]
-            person_names.each do |person_name|
-                new_text = raw_text.gsub(%r{#{person_name}}) do |match|
-                    "<b>#{match}</b>"
-                end
-                if raw_text != new_text
-                    raw_text = new_text
-                    break
-                end
-            end
+  class RenderBoldCiteTagBlock < Liquid::Block
+    def render(context)
+      raw_text = super
+      context.registers[:site].collections['people'].docs.each do |person|
+        person_names = [person.data['name']]
+        person_names += person.data['pub_names']
+        person_names.each do |person_name|
+          new_text = raw_text.gsub(/#{person_name}/) do |match|
+            "<b>#{match}</b>"
+          end
+          if raw_text != new_text
+            raw_text = new_text
+            break
+          end
         end
-
-        raw_text
       end
 
+      raw_text
     end
+  end
 end
 
 Liquid::Template.register_tag('render_bold_cite', Jekyll::RenderBoldCiteTagBlock)
--- _plugins/link_cite.rb
+++ _plugins/link_cite.rb
@@ -1,25 +1,24 @@
 module Jekyll
-    class RenderLinkCiteTagBlock < Liquid::Block
-      def render(context)
-        raw_text = super
-        context.registers[:site].collections["people"].docs.each do |person|
-            person_names = [person.data["name"]]
-            person_names += person.data["pub_names"]
-            person_names.each do |person_name|
-                new_text = raw_text.gsub(%r{#{person_name}}) do |match|
-                    "<a class=\"cite-link\" href=\"#{person.url}\">#{match}</a>"
-                end
-                if raw_text != new_text
-                    raw_text = new_text
-                    break
-                end
-            end
+  class RenderLinkCiteTagBlock < Liquid::Block
+    def render(context)
+      raw_text = super
+      context.registers[:site].collections['people'].docs.each do |person|
+        person_names = [person.data['name']]
+        person_names += person.data['pub_names']
+        person_names.each do |person_name|
+          new_text = raw_text.gsub(/#{person_name}/) do |match|
+            "<a class=\"cite-link\" href=\"#{person.url}\">#{match}</a>"
+          end
+          if raw_text != new_text
+            raw_text = new_text
+            break
+          end
         end
-
-        raw_text
       end
 
+      raw_text
     end
+  end
 end
 
 Liquid::Template.register_tag('render_link_cite', Jekyll::RenderLinkCiteTagBlock)
